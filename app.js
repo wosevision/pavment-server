7de73a7ae7ab8bbb39f4c38ee29b507a1502d59a
wosevision <jacksonteather@hotmail.com>
2016-03-09 23:46:08 -0500

    Disable CORS again

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cors = require('cors');
 var mongoose = require('mongoose');
 var env = require('node-env-file');
 
@@ -29,7 +28,6 @@ app.engine('html', require('hogan-express'));;
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
